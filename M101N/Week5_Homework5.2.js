e084cf10eb790034b241bf4c923dde6a3324e59e
MelnikovIG <zero_88@bk.ru>
2018-04-22 17:37:59 +0300

    Update Week5_Homework5.2.js

--- M101N/Week5_Homework5.2.js
+++ M101N/Week5_Homework5.2.js
@@ -1,23 +1,41 @@
 db.zips.aggregate([
-	//filter by state names
-	{$match:{$or:[{"state": "CA"},{"state": "NY"}]}},
-	//group by state and city
-	{$group:
-		{
-			_id:{
-					city:"$city",
-					state:"$state"
-				},
-			pop:{$sum:"$pop"}
-		}
-	},
-	//filter cities with pop > 25000
-	{$match:{pop:{$gt:25000}}},
-	//find average
-	{$group:
-		{
-			_id: null,
-			pop:{$avg:"$pop"}
-		}
-	}
+    //filter by state names
+    {
+        $match: {
+            $or: [{
+                "state": "CA"
+            }, {
+                "state": "NY"
+            }]
+        }
+    },
+    //group by state and city
+    {
+        $group: {
+            _id: {
+                city: "$city",
+                state: "$state"
+            },
+            pop: {
+                $sum: "$pop"
+            }
+        }
+    },
+    //filter cities with pop > 25000
+    {
+        $match: {
+            pop: {
+                $gt: 25000
+            }
+        }
+    },
+    //find average
+    {
+        $group: {
+            _id: null,
+            pop: {
+                $avg: "$pop"
+            }
+        }
+    }
 ])
