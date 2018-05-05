942fd0bf9c5723848ec215a6c24a278d42b9b30a
MelnikovIG <zero_88@bk.ru>
2018-05-06 00:19:13 +0300

    Create Week7_Homework7.7.js

---
+++ M101N/Week7_Homework7.7.js
@@ -0,0 +1,20 @@
+//create index for better search
+db.albums.createIndex({ "images": 1 });
+
+//iterate and remove unused images
+db.images.find().forEach(
+	function(current) {
+		var album = db.albums.findOne({"images":current._id})
+		
+		var haveAnyAlbum = album != null;
+		
+		if(haveAnyAlbum){
+			//print( "current: " + current._id + " album: " + album._id);
+		}
+		else{
+			print( "current: " + current._id + " NO ALBUM");
+			db.images.deleteOne({"_id":current._id})
+		}
+
+	}
+)
